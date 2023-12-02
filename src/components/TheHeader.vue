adc1715589c09120bed74c4b3a906bfd23bc8049
José R. Ferreyra <jferreyra.dev@gmail.com>
2023-12-02 13:57:49 -0300

    init

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <div class="banner">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="100" height="100" />
+      <div>
+        <h1>Administración de presupuestos</h1>
+        <h1>Municipalidad de Trancas</h1>
+      </div>
+    </div>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/cargapre">Carga de presupuesto</RouterLink>
+    </nav>
+  </div>
+</template>
+<style scoped>
+.wrapper {
+  display: flex;
+  place-items: flex-start;
+  flex-wrap: wrap;
+}
+.banner {
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    padding: 1rem;
+}
+</style>
---
+++ src/views/PresupuestoCarga.vue
@@ -0,0 +1,59 @@
+<script setup></script>
+
+<template>
+  <div>
+    <fieldset>
+      <legend>Datos principales</legend>
+      <label for="dcto">Decreto</label>
+      <input type="text" name="dcto" id="dcto" />
+      <label for="dte">Fecha</label>
+      <input type="date" name="" id="" />
+      <label for="dest">Destinatario</label>
+      <input type="text" name="dest" id="dest" />
+      <label for="concepto">Concepto</label>
+      <input type="text" name="concepto" id="dest" />
+    </fieldset>
+    <div class="inputlay">
+      <fieldset>
+        <legend>Retencinoes</legend>
+        <div class="inputbox">
+          <label for="desc">Observaciones</label>
+          <input type="text" name="desc" id="desc" />
+          <label for="porc">Porcentaje</label>
+          <input type="number" name="porc" id="porc" />
+          <label for="importe">Importe</label>
+          <input type="number" name="importe" id="importe" />
+        </div>
+        <label for="tot">Total</label>
+      </fieldset>
+      <div>
+        <table>
+          <tr>
+            <th>Descripcion</th>
+            <th>Importe</th>
+          </tr>
+          <tr>
+            <td>Importe a descontar en concepto de utileria</td>
+            <td>10000</td>
+          </tr>         
+        </table>
+        <button>Ingresar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.boxform {
+  border: solid 1px gainsboro;
+  border-radius: 1rem;
+  padding: 1rem;
+}
+.inputbox {
+  display: flex;
+  flex-direction: column;
+}
+.inputlay {
+  display: flex;
+}
+</style>
