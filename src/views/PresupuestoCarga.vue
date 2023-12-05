3bd291445cc136ad2b1ea7dcbc3f46d9a4edc56a
José R. Ferreyra <jferreyra.dev@gmail.com>
2023-12-04 23:57:32 -0300

    demo carga

--- src/views/PresupuestoCarga.vue
+++ src/views/PresupuestoCarga.vue
@@ -1,10 +1,33 @@
 <script setup>
 import AppModal from '../components/AppModal.vue'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import data from '../assets/presup.json'
 
 const showInnerForm = ref(false)
 const selected = ref('')
+const selectedItems = ref([])
+
+const total = computed(() =>
+  selectedItems.value.reduce(
+    (accumulator, currentValue) => accumulator + Number(currentValue.importe),
+    0
+  )
+)
+
+const handleSelect = (item) => {
+  if (item['HIJO'] === 1) {
+    let importe
+    do {
+      importe = 0
+      importe = prompt('Ingrese el importe (9999.99)', importe)
+    } while (isNaN(importe))
+    //importe = Number(importe).toFixed(2)  id: Date.now(),
+    selectedItems.value.push({
+      item,
+      importe: importe
+    })
+  }
+}
 </script>
 
 <template>
@@ -57,20 +80,51 @@ const selected = ref('')
     </div>
   </div>
   <AppModal :show="showInnerForm" title="Carga preliminar" @close="showInnerForm = false">
-    
-      <div>
-        <div class="p-2 m-2">
-          <input type="text" name="search" id="search" placeholder="Busqueda" />
-        </div>
-        <div class="max-h-72 overflow-auto text-black">
-          <select size="5" v-model="selected">
-            <option v-for="item in data" :key="item.IDDETPRE" :disabled = "item.HIJO==0?true:false" >
-              {{ item.CUENTA }} - {{ item.DESCRIPCION }}
-            </option>
-          </select>
-        </div>
+    <div>
+      <div class="p-2 m-2">
+        <input type="text" name="search" id="search" placeholder="Busqueda" />
       </div>
+      <div class="max-h-72 overflow-auto text-black">
+        <select size="5" v-model="selected">
+          <option
+            v-for="item in data"
+            :key="item.IDDETPRE"
+            :disabled="item.HIJO == 0 ? true : false"
+            @dblclick="handleSelect(item)"
+            @keyup.enter="handleSelect(item)"
+          >
+            {{ item.CUENTA }} - {{ item.DESCRIPCION }}
+          </option>
+        </select>
+      </div>
+    </div>
     <span>Tabla de cargas con importe preliminar</span>
+    <table v-if="selected.length>0" >
+      <thead>
+        <tr class="bg-blue-400">
+          <th class="border text-left px-2 py-1">Cuenta</th>
+          <th class=" border text-left px-2 py-1">Descripción</th>
+          <th class=" border text-left px-2 py-1">Importe</th>
+          <th class=" border text-left px-2 py-1">Acción</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="hover:bg-blue-400" v-for="el in selectedItems" :key="el.item.IDDETPRE">
+          <td>{{ el.item.CUENTA }}</td>
+          <td>{{ el.item.DESCRIPCION }}</td>
+          <td class="text-right">{{ el.importe }}</td>  
+          <td>  </td>        
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr class="bg-blue-400">
+          <td class="border text-right py-1"></td>
+          <td class="border text-right py-1">Total</td>
+          <td class="border text-right py-1">{{ total }}</td>
+          <td class="border text-right py-1"></td>
+        </tr>
+      </tfoot>
+    </table>
   </AppModal>
 </template>
 
